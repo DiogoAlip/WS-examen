22a518133b8fa3e7d0e1c3312d27d4313a155b1f
Diogo <diogoalipazaga@gmail.com>
2024-01-15 19:53:38 -0500

    feat: new client alumno

--- app.js
+++ app.js
@@ -6,12 +6,24 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let notasData = Array(arrAlumnos.length).fill("none");
+
 io.on("connection", (socket) => {
   console.log("Un usuario se ha conectado");
+
+  socket.on("submitNotes", (newNotasData) => {
+    notasData = [...newNotasData];
+  });
+
+  socket.emit("initialData", notasData);
 });
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/profesor/index.html`);
+});
+
+app.get("/alumno", (req, res) => {
+  res.sendFile(`${__dirname}/alumno/index.html`);
 });
 
 server.listen(3060, () => {
